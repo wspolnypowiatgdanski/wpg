618628972f1be2978dda18275d3af7fa351468d8
wspolnypowiatgdanski <wspolnypowiatgdanski@gmail.com>
2024-03-01 08:30:05 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <b>KOMITET WYBORCZY WYBORCÓW WSPÓLNY POWIAT GDAŃSKI</b>
+        <br />
+        Wybory samorządowe 2024
+      </header>
+      <div className="App-body">
+        <a
+          href="https://docs.google.com/document/d/1kwpwXBVhe1P7DCGBSvm5V6OtIqZFhIOkBidd6MKagGA/edit?usp=sharing"
+          target="blank"
+        >
+          Rejestr zaciągniętych kredytów
+        </a>
+        <br />
+        <a
+          href="https://docs.google.com/document/d/1w56yN1iGysshzSOWe8PI2OQvXJs8n4fcZ4cHmbrDDbA/edit?usp=sharing"
+          target="blank"
+        >
+          Rejestr wpłat o wartości przekraczającej łącznie 4242,00 zł
+        </a>
+      </div>
+      <footer className="App-footer">
+        <div>
+          <p>
+            <b>KOMITET WYBORCZY WYBORCÓW WSPÓLNY POWIAT GDAŃSKI</b>
+          </p>
+          <p>
+            ul. Kasztanowa 21/4
+            <br />
+            Bąkowo
+            <br />
+            83-050 Kolbudy
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
